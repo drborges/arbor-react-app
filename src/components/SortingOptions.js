5ebc8c6cab586bea5ca346984a52bceacb413bd0
Diego Borges <drborges.cic@gmail.com>
2017-12-23 20:36:15 -0200

    Wrap text within JSX expression

--- src/components/SortingOptions.js
+++ src/components/SortingOptions.js
@@ -3,9 +3,9 @@ import React from "react"
 const SortingOptions = ({ todos }) => (
   <div>
     {'Sort By: '}
-    <button onClick={todos.sortByTitle}>Title</button>
-    <button onClick={todos.sortByCompleteness}>Done?</button>
-    <button onClick={todos.reverse}>Reverse</button>
+    <button onClick={todos.sortByTitle}>{'Title'}</button>
+    <button onClick={todos.sortByCompleteness}>{'Done?'}</button>
+    <button onClick={todos.reverse}>{'Reverse'}</button>
   </div>
 )
 
