26af574713549f24776ed50733850cf820f5ca48
Diego Borges <drborges.cic@gmail.com>
2017-12-23 22:14:14 -0200

    Simplify Timecontrol's JSX

--- src/components/Timecontrol.js
+++ src/components/Timecontrol.js
@@ -1,23 +1,21 @@
 import React from "react"
 
-const Timecontrol = ({ timeline, recorder }) => {
-  return (
-    <div className="time-control">
-      <div>
-        <button onClick={() => timeline.travel.step(-1)}>{'⬅️'}</button>
-        <button onClick={() => recorder.toggle(timeline)}>{recorder.recording ? '⏹️' : '⏺️'}</button>
-        <span>{`Time Cursor: ${timeline.cursor}`}</span>
-        <button onClick={() => timeline.travel.step(1)}>{'➡️'}</button>
-      </div>
-      <input
-        type="range"
-        min={0}
-        max={timeline.history.length - 1}
-        value={timeline.cursor}
-        onChange={(event) => timeline.travel.to(event.target.valueAsNumber)}
-      />
+const Timecontrol = ({ timeline, recorder }) => (
+  <div className="time-control">
+    <div>
+      <button onClick={() => timeline.travel.step(-1)}>{'⬅️'}</button>
+      <button onClick={() => recorder.toggle(timeline)}>{recorder.recording ? '⏹️' : '⏺️'}</button>
+      <span>{`Time Cursor: ${timeline.cursor}`}</span>
+      <button onClick={() => timeline.travel.step(1)}>{'➡️'}</button>
     </div>
-  )
-}
+    <input
+      type="range"
+      min={0}
+      max={timeline.history.length - 1}
+      value={timeline.cursor}
+      onChange={(event) => timeline.travel.to(event.target.valueAsNumber)}
+    />
+  </div>
+)
 
 export default Timecontrol
